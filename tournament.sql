3ab4f17013da0cf77ec3baae0c8034d40cc6b447
Farong Cheng <cfr009@163.com>
2015-05-27 20:17:17 -0400

    modify sql

--- tournament.sql
+++ tournament.sql
@@ -10,3 +10,36 @@
 CREATE TABLE players ( name TEXT,
                      createtime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                      id SERIAL );
+
+CREATE TABLE games ( name TEXT,
+					 createtime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+					 id SERIAL );
+					 
+CREATE TABLE matches ( player1id SERIAL,
+					 player2id SERIAL,
+					 gameid SERIAL,
+					 winner SERIAL,
+					 createtime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+					 id SERIAL );
+					 
+				
+CREATE VIEW playermatches AS
+SELECT players.id, players.name, player1id, player2id, gameid, winner
+FROM players, matches
+WHERE players.id = matches.player1id OR players.id = matches.player2id
+
+CREATE VIEW wins AS
+SELECT id, count(*) as wins
+FROM playermatches
+WHERE id = winner
+GROUP BY id
+
+CREATE VIEW totalmatches AS
+SELECT id, count(*) as matches
+FROM playermatches
+GROUP BY id
+
+CREATE VIEW standing AS
+SELECT players.id, players.name, wins, matches
+FROM wins, totalmatches, players
+WHERE wins.id = totalmatches.id
